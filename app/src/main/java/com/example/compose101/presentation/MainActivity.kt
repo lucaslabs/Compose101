1f04cc925846c3371a0012398bf9b05e2c95915f
Lucas Nobile <lucas.nobile@strike.me>
2022-10-12 15:26:50 -0300

    Add composable to show text, button and counter

--- app/src/main/java/com/example/compose101/presentation/MainActivity.kt
+++ app/src/main/java/com/example/compose101/presentation/MainActivity.kt
@@ -4,6 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose101.presentation.theme.Compose101Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,5 +30,26 @@ class MainActivity : ComponentActivity() {
                 ReverseTextScreen(viewModel)
             }
         }
+    }
+
+    @Composable
+    fun ClickTheButton() {
+        val counter = remember { mutableStateOf(0) }
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            Text(text = "Button clicked ${counter.value} times")
+            Button(onClick = { counter.value++ }) {
+                Text(text = "Click me")
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun Preview() {
+        ClickTheButton()
     }
 }
